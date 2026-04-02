08385379720bc430a04c77799ad8a765d5ccfc2d
chankurjawla <144441133+chankurjawla@users.noreply.github.com>
2026-04-02 09:28:29 +0000

    dbjsdf

--- app_pdf_merge.py
+++ app_pdf_merge.py
@@ -5,30 +5,20 @@ import os
 st.subheader('Merge PDF files!')
 #st.write('Select pdfs to be merged.')
 pdf_files = st.file_uploader('upload files to be merged',type=["pdf","PDF"],accept_multiple_files=True, key=None)
-
+output_filename = 'merged_document.pdf'
 def mergepdf():
     merger = PdfMerger()
     # Check if input files exist before merging
     all_files_exist = True
     for pdf_file in pdf_files:
-        if not os.path.exists(pdf_file):
-            print(f"Error: File not found: {pdf_file}")
-            all_files_exist = False
-            break
-
-    if all_files_exist:
-        # Append each PDF to the merger
         for pdf_file in pdf_files:
-            merger.append(pdf_file)
+        merger.append(pdf_file)
 
-        # Write the merged PDF to an output file
-        with open(output_filename, 'wb') as output_pdf:
-            merger.write(output_pdf)
-
+    # Write the merged PDF to an output file
+    with open(output_filename, 'wb') as output_pdf:
+        merger.write(output_pdf)
         merger.close()
-        return output_pdf
-    else:
-        print("Merging aborted due to missing file(s).")
+    return output_pdf
 
 if st.button('Merge pdf'):
     mergepdf()
