e5e24d9d6a044495f2d93f1bbac887a4e2b07c28
chankurjawla <144441133+chankurjawla@users.noreply.github.com>
2026-04-02 09:18:09 +0000

    mhvjhv

--- app_pdf_merge.py
+++ app_pdf_merge.py
@@ -2,5 +2,33 @@ import streamlit as st
 from PyPDF2 import PdfMerger
 
 st.subheader('Merge PDF files!')
-st.write('Select pdfs to be merged.')
-pdf_files = st.file_uploader('upload pdf siles here',type=["pdf","PDF"],accept_multiple_files=True, key=None)
+#st.write('Select pdfs to be merged.')
+pdf_files = st.file_uploader('upload files to be merged',type=["pdf","PDF"],accept_multiple_files=True, key=None)
+
+def mergepdf():
+    merger = PdfMerger()
+    # Check if input files exist before merging
+    all_files_exist = True
+    for pdf_file in pdf_files:
+        if not os.path.exists(pdf_file):
+            print(f"Error: File not found: {pdf_file}")
+            all_files_exist = False
+            break
+
+    if all_files_exist:
+        # Append each PDF to the merger
+        for pdf_file in pdf_files:
+            merger.append(pdf_file)
+
+        # Write the merged PDF to an output file
+        with open(output_filename, 'wb') as output_pdf:
+            merger.write(output_pdf)
+
+        merger.close()
+    else:
+        print("Merging aborted due to missing file(s).")
+
+if st.buttin():
+    mergepdf()
+    st.rerun()
+st.download_button('Download merged file',output_pdf)
